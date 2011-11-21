4a9b2916f7a992d9d6aec025ebefe4f0ed016ca3
Collette Grabill <ces84@drexel.edu>
2011-11-21 09:53:10 -0500

    while.rb

---
+++ while.rb
@@ -0,0 +1,34 @@
+puts 'Hello, and what is your name?'
+name=gets.chomp
+
+if 
+ name == name.capitalize
+puts 'access granted'
+else
+ puts name + '? You should write your name properly'
+ puts 'Doesn\'t your name start with a capital Letter?'
+ reply = gets.chomp
+ if reply.downcase =='yes'
+  puts 'Then write it that way'
+ else
+ end
+ puts 'Want to try again?'
+ reply=gets.chomp
+ if reply=='yes'
+ puts 'type your name'
+ name == gets.chomp
+   if name == name.capitalize
+     puts 'access granted'
+   else
+     puts 'access denied'
+     puts 'I\'ll help you out'
+     puts 'your name should be typed like this'
+     puts '* ' + name.capitalize + ' *'
+   end
+ else
+ name != name.capitalize
+ puts 'access denied'
+ end
+end
+  
+
